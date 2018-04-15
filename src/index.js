68034ed1294ba7c15d11f697632d275bd9a5cee6
Fenreliania <38388535+Fenreliania@users.noreply.github.com>
2018-04-15 10:11:50 +1000

    Finished clearing up template code

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import { render } from 'react-dom';
 
 
 const App = () => (
-  <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
+  <div>
   </div>
 );
 
