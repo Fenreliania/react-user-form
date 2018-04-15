899a12c5e8da30dae2b3c3cebfe72b48ef62b3e7
Fenreliania <38388535+Fenreliania@users.noreply.github.com>
2018-04-15 18:47:10 +1000

    Completed to a reasonable degree. Approx time spent: 5 hours

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import UserForm from "./UserDetails";
 
+// Complete
 render(
   <UserForm className="col-6 offset-3" />,
   document.getElementById("root")
