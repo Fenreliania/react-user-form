22d91fc43c83542331444089b769139b060668ef
Fenreliania <38388535+Fenreliania@users.noreply.github.com>
2018-04-15 13:22:05 +1000

    Added labels to form components, to maintain consistent formatting while allowing multiple instances of the same component to have different labels.

--- src/UserDetails.js
+++ src/UserDetails.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const NameField = ({ name, value, onValidate, ...props }) => {
+const NameField = ({ name, value, onValidate, label, ...props }) => {
   const validate = event => {
     // Agnostic of characters, wasn't sure if >2 names was desired
     var valid = event.target.value.trim().split(" ").length === 2;
@@ -8,17 +8,20 @@ const NameField = ({ name, value, onValidate, ...props }) => {
   };
 
   return (
-    <input
-      type="text"
-      value={value}
-      onChange={validate}
-      name={name}
-      {...props}
-    />
+    <React.Fragment>
+      <label for={name}>{label}</label>
+      <input
+        type="text"
+        value={value}
+        onChange={validate}
+        name={name}
+        {...props}
+      />
+    </React.Fragment>
   );
 };
 
-const ContactFieldGroup = ({ name, value, onValidate, ...props }) => {
+const ContactFieldGroup = ({ name, value, onValidate, label, ...props }) => {
   const validate = event => {
     // Need to figure out how to structure this to pass the event
     //var valid = !!values.type && !!values.value;
@@ -39,10 +42,21 @@ const ContactFieldGroup = ({ name, value, onValidate, ...props }) => {
     );
   }
 
-  return <div>{fields}</div>;
+  const addContact = event => {
+    // Not quite sure how to handle this while maintaining immutable state
+    //value.append
+  };
+
+  return (
+    <React.Fragment>
+      <label>{label}</label>
+      <div>{fields}</div>
+      <button onClick={addContact} value="Add Contact" />
+    </React.Fragment>
+  );
 };
 
-const ContactField = ({ name, value, onValidate, ...props }) => {
+const ContactField = ({ name, value, onValidate, label, ...props }) => {
   const validate = event => {
     var valid = event.target.value.trim().split(" ").length === 2;
     onValidate(event, valid);
@@ -50,13 +64,14 @@ const ContactField = ({ name, value, onValidate, ...props }) => {
 
   return (
     <div>
+      <label>Number</label>
       <input type="text" value={value.type} onChange={validate} {...props} />
       <input type="text" value={value.value} onChange={validate} {...props} />
     </div>
   );
 };
 
-const DOBField = ({ name, value, onValidate, ...props }) => {
+const DOBField = ({ name, value, onValidate, label, ...props }) => {
   const validate = event => {
     // Get the date 18 years ago
     var thresholdDate = new Date();
@@ -65,20 +80,34 @@ const DOBField = ({ name, value, onValidate, ...props }) => {
     onValidate(event, valid);
   };
 
-  return <input type="date" value={value} onChange={validate} {...props} />;
+  return (
+    <React.Fragment>
+      <label for={name}>{label}</label>
+      <input
+        name={name}
+        type="date"
+        value={value}
+        onChange={validate}
+        {...props}
+      />
+    </React.Fragment>
+  );
 };
 
-const GenderField = ({ name, value, onValidate, ...props }) => {
+const GenderField = ({ name, value, onValidate, label, ...props }) => {
   const validate = event => {
     onValidate(event, true);
   };
 
   return (
-    <select name={name} value={value} onChange={validate}>
-      <option value="0">Other/Prefer not to say</option>
-      <option value="1">Female</option>
-      <option value="2">Male</option>
-    </select>
+    <React.Fragment>
+      <label for={name}>{label}</label>
+      <select name={name} value={value} onChange={validate}>
+        <option value="0">Other/Prefer not to say</option>
+        <option value="1">Female</option>
+        <option value="2">Male</option>
+      </select>
+    </React.Fragment>
   );
 };
 
@@ -123,22 +152,27 @@ class UserForm extends React.Component {
           name="name"
           value={this.state.name}
           onValidate={this.onValidate.bind(this)}
+          label="Full Name"
         />
         <DOBField
           name="dob"
           value={this.state.dob}
           onValidate={this.onValidate.bind(this)}
+          label="Date of Birth"
         />
         <GenderField
           name="gender"
           value={this.state.gender}
           onValidate={this.onValidate.bind(this)}
+          label="Gender"
         />
         <ContactFieldGroup
           name="contact"
           value={this.state.contact}
           onValidate={this.onValidate.bind(this)}
+          label="Contact Numbers"
         />
+        <label for="hasGuardian">Requires Guardian Consent?</label>
         <input
           type="checkbox"
           name="hasGuardian"
@@ -151,11 +185,13 @@ class UserForm extends React.Component {
               name="guardianName"
               value={this.state.guardianName}
               onValidate={this.onValidate.bind(this)}
+              label="Guardian Name"
             />
             <ContactFieldGroup
               name="guardianContact"
               value={this.state.guardianContact}
               onValidate={this.onValidate.bind(this)}
+              label="Guardian Contact Numbers"
             />
           </div>
         ) : (
